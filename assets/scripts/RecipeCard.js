fc6baf74e0541f4fd77f29cbbf4819903d985381
yunzexie <yux028@ucsd.edu>
2023-05-11 23:11:32 -0700

    Update RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -122,12 +122,13 @@ class RecipeCard extends HTMLElement {
         if (!data) return;
 
         // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-        let article = this.shadowRoot.querySelector('articles');
+        let article = this.shadowRoot.querySelector('article');
         // A7. TODO - Set the contents of the <article> with the <article> template given in
         //           cardTemplate.html and the data passed in (You should only have one <article>,
         //           do not nest an <article> inside another <article>). You should use Template
         //           literals (tempalte strings) and element.innerHTML for this.
         article.innerHTML = `
+          ${data.imgSrc}
           alt="Recipe Title">
           <p class="title">
             <a href="https://link-to-article.com">Title</a>
